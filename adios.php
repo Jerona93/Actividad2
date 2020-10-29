684593a0b1abcaa7a6ac77d7a5f708dd4ae6ccb5
jerona <srcololo@gmail.com>
2020-10-29 14:07:47 +0100

    archivo correcto adios.php

---
+++ adios.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html >
+    <head>
+        <meta charset="UTF-8">
+        <title>Segundo PHP</title>
+    </head>
+    <body>
+     <?php echo"Hasta luego desarrollador!!!"?>
+    </body>
+</html>
